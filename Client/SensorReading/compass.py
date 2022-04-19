8281058e227c701d25bb26a8453d39c0e01d99bd
brandon-red <brandonred627@gmail.com>
2022-04-18 23:23:21 -0400

    setup sensor code in folder to follow online template

---
+++ Client/SensorReading/compass.py
@@ -0,0 +1,18 @@
+######################################################################################
+                # Raspberry Pi source code for sensing electronic compass #
+# Should be structured as a function that can be called from main and other processes #
+                # Look at encoder.py as a template example #
+######################################################################################
+
+import RPi.GPIO as GPIO
+import time
+
+class Compass:
+    def __init__():
+        pass
+    
+    def setup():
+        pass
+
+    def getPosition():
+        pass
