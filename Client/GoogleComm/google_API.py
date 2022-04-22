fb5f9b7c71da4373754e92370d66ca2bb0c30d00
brandon-red <brandonred627@gmail.com>
2022-04-22 00:20:41 -0400

    Moved google API code to respective folder within Client directory

---
+++ Client/GoogleComm/google_API.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import RPi.GPIO as GPIO
+import json
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+app = Flask(__name__)
+
+
+@app.route('/prime', methods=['POST'])
+def prime():
+    content = request.get_json() #JSON
+    content['valid'] = True
+    f = open("request.txt", 'w')
+    f.write(json.dumps(content))
+    f.close()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4444, host='0.0.0.0')
