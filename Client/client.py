4d476b604c5a491f9c45cc9736568d21941153af
giuliontini <giulio_fiorentini@hotmail.com>
2022-04-26 16:31:52 -0400

    Fixing bugs when importing sensor classes and changing pin values from BOARD to BCM numbering schemes

--- Client/client.py
+++ Client/client.py
@@ -1,26 +1,12 @@
-"""
-Client program
 
-
-How To Run
-------------
-
-python3 Client.py -p <a port number> [-s server ip]
-
-E.g. 
-python3 Client.py -p 1234 -s 192.168.1.10       # connect server at 192.168.1.10 port 1234
-python3 Client.py -p 1234 -s raspberrypi.local  # connect server at raspberrypi.local port 1234
-python3 Client.py -p 1234                       # connect server at 127.0.0.1 port 1234
-
-"""
 
 import socket
 import json
 import time
-from Client.SensorReading.compass import Compass
-from Client.SensorReading.encoder import Encoder
-from Client.SensorReading.infrared import Infrared
-from Client.SensorReading.ranger import Ranger
+from SensorReading.compass import Compass
+from SensorReading.encoder import Encoder
+from SensorReading.infrared import Infrared
+from SensorReading.ranger import Ranger
 import RPi.GPIO as GPIO
 
 """
@@ -34,12 +20,12 @@ valid_commands = {"go forward", "go backward", "turn left",
     "turn right", "go to A", "go to B", "go to C"}
 
 # Front Ranger Pin Setup
-TRIG1 = 16 
-ECHO1 = 11
+TRIG1 = 23 
+ECHO1 = 17
 
 # Back Ranger Pin Setup
-TRIG2 = 18
-ECHO2 = 13
+TRIG2 = 24
+ECHO2 = 27
 
 # Compass Setup
 
@@ -47,9 +33,8 @@ ECHO2 = 13
 
 # IR Setup
 
-IR_DATA1 = 15
-
-IR_DATA2 = 22
+IR_FRONT = 15
+IR_BACK = 25
 
 # Encoder Setup
 """
@@ -62,23 +47,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.cleanup()
 
-rangerF = Ranger(TRIG1, ECHO1)
-rangerF.setup()
 
-rangerB = Ranger(TRIG2, ECHO2)
-rangerB.setup()
-
-encoder_obj = Encoder(CLK, DT)
-encoder_obj.setup()
-
-infraredF = Infrared(IR_DATA1)
-infraredF.setup()
-
-infraredB = Infrared(IR_DATA2)
-infraredB.setup()
-
-compass_obj = Compass()
-compass_obj.setup()
 
 port = 44444
 port = "127.0.0.1"
@@ -135,3 +104,21 @@ def send_packet(port, ip):
         sock.close()
         return True
     
+if __name__ == "__main__":
+    rangerF = Ranger(TRIG1, ECHO1)
+    rangerF.setup()
+
+    rangerB = Ranger(TRIG2, ECHO2)
+    rangerB.setup()
+
+    encoder_obj = Encoder(CLK, DT)
+    encoder_obj.setup()
+
+    infraredF = Infrared(IR_DATA1)
+    infraredF.setup()
+
+    infraredB = Infrared(IR_DATA2)
+    infraredB.setup()
+
+    compass_obj = Compass()
+    compass_obj.setup()
