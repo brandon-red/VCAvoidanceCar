21e8f4fd58c045a5bac46ad64d6fe3d43e7330c4
brandon-red <brandonred627@gmail.com>
2022-04-29 12:48:12 -0400

    Added Obstacle Avoidance code

---
+++ Client/ObstacleAvoidance/obstacleDetection.py
@@ -0,0 +1,14 @@
+from SensorReading.infrared import Infrared
+from SensorReading.ranger import Ranger
+
+def checkFront(rangerF):
+    pass
+
+def checkBack(rangerB):
+    pass
+
+def checkRight(rangerR):
+    pass
+
+def checkLeft(rangerL):
+    pass
